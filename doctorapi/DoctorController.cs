f7a774b6002cb03a27cc6f41a18be9c4ddf8d203
Survesh Rathore <113424413+SurveshRathore@users.noreply.github.com>
2023-04-07 13:21:19 +0530

    DoctorController.cs

---
+++ doctorapi/DoctorController.cs
@@ -0,0 +1,67 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Model;
+using Experimental.System.Messaging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    [ApiController]
+    [Route("doctor")]
+    public class DoctorController:ControllerBase
+    {
+        private IdoctorBL idoctorBL;
+
+        public DoctorController (IdoctorBL idoctorBL)
+        {
+            this.idoctorBL = idoctorBL;
+        }
+
+        [HttpPost]
+        [Route("addDoctor")]
+        public IActionResult addDoctor(doctorTable doctorTable)
+        {
+            try
+            {
+                var result = this.idoctorBL.addNewDoctor(doctorTable);
+
+                if(result != null)
+                {
+                    return this.Ok(new { success = true, Message = "Doctor is added", Response = result });
+                }
+                else
+                {
+                    return this.BadRequest(new { success = false, Message = "unable to add Doctor " });
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("getDoctor")]
+        public IActionResult getDoctor()
+        {
+            try
+            {
+                var result = this.idoctorBL.getAllDoctor();
+
+                if (result != null)
+                {
+                    return this.Ok(new { success = true, Message = "successfully fetch the Doctor", Response = result });
+                }
+                else
+                {
+                    return this.BadRequest(new { success = false, Message = "unable to fetch Doctor " });
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
